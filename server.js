670a58f6c6bb861c8d73924d96997a150d8b71c0
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-09-02 20:13:08 -0600

    server now listens on port

--- server.js
+++ server.js
@@ -23,3 +23,7 @@ app.get('*', (req, res) =>
 app.get('/notes',(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/notes.html'))
 });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
