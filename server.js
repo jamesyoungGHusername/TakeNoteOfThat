e3d6660c49d54c74e099cc776befb7646539320e
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-09-02 20:00:11 -0600

    changed file directory to play nice with heroku

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.get('/', (req, res) =>
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
 app.get('/notes',(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/notes.html'))
 });
