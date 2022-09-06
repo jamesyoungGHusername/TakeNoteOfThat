d0b6d7822d5855dbc880cf6d1bd698d6a2858ba6
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-09-05 23:41:15 -0600

    potential heroku fix (can't handle * wildcard format

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ app.use(express.static('../Client/public/'));
 
 // GET Route for homepage
 
-app.get('*', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '../Client/public/index.html'))
 );
 app.get('/notes.html',(req,res)=>{
